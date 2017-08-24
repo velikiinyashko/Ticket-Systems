182e9fb3e210f6a164dc8fdd5293b8bec903468e
chii chobits <velikiinyashko@live.ru>
2017-08-24 21:37:05 +0700

    Update 24.08.2017/2

---
+++ SystemsLogic/ORM/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemsLogic.ORM
+{
+    interface IRepository<T> : IDisposable
+        where T : class
+    {
+        IEnumerable<T> GetDataList(); // Получение всех пользователей
+        T GetUser(int Id); // Получение пользователя по ID
+        void Create(T Item); // Создание пользователя
+        void Update(T Item); // Обнавление 
+        void Delete(int Id); // Удаление пользователя по ID
+        void Save(); // Запись даных в базу
+    }
+}
---
+++ SystemsLogic/ORM/Repository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+
+namespace SystemsLogic.ORM
+{
+
+    class Repository
+    {
+        private Context Db;
+
+        public Repository(string ConnectionString)
+        {
+            this.Db = new Context(ConnectionString);
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            return Db.Users;
+        }
+
+        public User GetUser(int Id)
+        {
+            return Db.Users.Find(Id);
+        }
+
+        public void Create(User user)
+        {
+            Db.Users.Add(user);
+        }
+
+        public void Update(User user)
+        {
+            Db.Entry(user).State = System.Data.Entity.EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            Db.SaveChanges();
+        }
+    }
+
+}
